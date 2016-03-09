e6e69dfd384324b731bda18efd7bb8b38dd5dc16
Mike Sovic <osu6672@oakley02.osc.edu>
2016-03-09 10:15:14 -0500

    Fixed issue with brace expansion using eval function

--- PowerAnalyses/Simulate_SFS.sh
+++ PowerAnalyses/Simulate_SFS.sh
@@ -2,13 +2,11 @@
 
 #Run this script with  ./Simulate_SFS.sh par_file_name [# of sim datasets to generate]
 #needs a par file that is indicated as the first argument on the command line and the fsc252 executable.
-#for now, still need to updated the value in the brace expansion in the line 'for sim in {1..X}'. Need to fix this.
-#it should match the second argument given on the command line (the num of simulations to perform).
 
 if [ $2 ]; then
     mkdir SimulatedSFS
 
-    for sim in {1..10}; do
+    for sim in $(eval echo "{1..$2}"); do
       mkdir sim$sim
       cp fsc252 *.par sim$sim
       cd sim$sim
